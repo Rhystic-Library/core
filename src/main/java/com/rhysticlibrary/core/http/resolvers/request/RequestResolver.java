0b2043b4cbd66ae17ae45b3bc0d212376a6c4966
Steven Timothy <steven.timothy.cp@gmail.com>
2025-03-04 22:30:08 -0700

    RL-70 lint fixes

--- src/main/java/com/rhysticlibrary/core/http/resolvers/request/RequestResolver.java
+++ src/main/java/com/rhysticlibrary/core/http/resolvers/request/RequestResolver.java
@@ -7,17 +7,28 @@ import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Resolver for interacting with the request.
+ */
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public final class RequestResolver {
 
   private final RequestAttributeResolver requestAttributeResolver;
 
+  /**
+   * Facade for setting attributes on the request.
+   *
+   * @param request The request for setting the attribute on.
+   * @param entity  The attribute to set.
+   * @param value   The value to set for the attribute.
+   * @param <T>     The type of the attribute.
+   */
   public <T> void setAttribute(
       @NonNull HttpServletRequest request,
-      @NonNull HttpEntity attribute,
+      @NonNull HttpEntity entity,
       T value
   ) {
-    this.requestAttributeResolver.setAttribute(request, attribute, value);
+    this.requestAttributeResolver.setAttribute(request, entity, value);
   }
 }
