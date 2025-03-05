5ce90d51b3535722be86888359547eafda66624b
Steven Timothy <steven.timothy.cp@gmail.com>
2025-03-04 22:06:05 -0700

    RL-69 Update build config; add HTTP attr resolver and test config updates (#2)

--- src/test/java/com/rhysticlibrary/core/BaseComponent.java
+++ src/test/java/com/rhysticlibrary/core/BaseComponent.java
@@ -1,10 +1,15 @@
 package com.rhysticlibrary.core;
 
 import com.rhysticlibrary.core.config.TestApplicationConfig;
+import com.rhysticlibrary.core.generators.common.TimestampGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(classes = TestApplicationConfig.class)
 @SpringBootTest
 public abstract class BaseComponent {
+
+  @Autowired
+  protected TimestampGenerator timestampGenerator;
 }
--- src/test/java/com/rhysticlibrary/core/config/TestApplicationConfig.java
+++ src/test/java/com/rhysticlibrary/core/config/TestApplicationConfig.java
@@ -1,9 +1,37 @@
 package com.rhysticlibrary.core.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.rhysticlibrary.core.modules.modelmapper.InstantModule;
+import com.rhysticlibrary.core.serializers.objectmapper.InstantSerializer;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Instant;
 
 @ComponentScan(basePackages = "com.rhysticlibrary")
 @Configuration
 public class TestApplicationConfig {
+
+  @Bean
+  public ModelMapper modelMapper(InstantModule instantModule) {
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.getConfiguration()
+        .setDeepCopyEnabled(true)
+        .setFieldMatchingEnabled(true)
+        .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+    modelMapper.registerModule(instantModule);
+
+    return modelMapper;
+  }
+
+  @Bean
+  public ObjectMapper objectMapper(InstantSerializer instantSerializer) {
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(Instant.class, instantSerializer);
+    return new ObjectMapper()
+        .registerModule(javaTimeModule);
+  }
 }
